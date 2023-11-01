c7f25c7812ef0adc61653914832397a5eb7e1c69
Alin Sarbu <alin.sarbu@student.unitbv.ro>
2023-11-01 20:44:16 +0200

    Modificare clasa Cell - date si metode

---
+++ projectMC++/projectMC++/Cell.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <cstdint>
+
+class Cell
+{
+private:
+
+	bool m_ocupied;
+	uint8_t m_x;
+	uint8_t m_y;
+	bool m_isMined;
+	bool m_isBulldozered;
+
+public:
+	Cell();
+	Cell(bool, uint8_t, uint8_t, bool, bool);
+};
