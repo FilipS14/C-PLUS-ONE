7c3b12313ec49a1ea303f0c90e46e767487cddc7
Alin Sarbu <alin.sarbu@student.unitbv.ro>
2023-11-01 20:00:02 +0200

    Modificare constructori si initializare metoda ocupied_cell

---
+++ Project1/Project1/Project1/Cell.cpp
@@ -0,0 +1,8 @@
+#include "Cell.h"
+
+Cell::Cell() : m_ocupied{ false }, m_x{ 0 }, m_y{ 0 }, m_isMined{ false }, m_isBulldozered{ false } {};
+
+Cell::Cell(bool ocupat, uint8_t x, uint8_t y, bool este_minat, bool este_lovit) : m_ocupied{ ocupat }, m_x{ x }, m_y{ y }, m_isMined{ este_minat }, m_isBulldozered{ este_lovit } {}
+
+bool Cell::ocupied_cell() const { return false; }
+
