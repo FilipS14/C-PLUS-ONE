17bcf75e4511f12af1d171534b1c611545ff8151
FilipS14 <filipsirboiu@yahoo.ro>
2023-11-04 15:33:41 +0200

    Am definit pentru clasa Pillar: -seteri -functia isValid

--- projectMC/projectMC/Pillar.cpp
+++ projectMC/projectMC/Pillar.cpp
@@ -23,3 +23,32 @@ uint8_t Pillar::getCoordY() const {
 Team Pillar::getTeam() const {
 	return m_team;
 }
+
+void Pillar::setCoordX(uint8_t coordX) {
+	if (m_coordX > coordX) {
+		std::cerr << "Invalid change to pillar\n";
+		return;
+	}
+	m_coordX = coordX;
+}
+
+void Pillar::setCoordY(uint8_t coordY) {
+	if (m_coordY > coordY) {
+		std::cerr << "Invalid change to pillar";
+		return;
+	}
+	m_coordY = coordY;
+}
+
+void Pillar::setTeam(Team team)
+{
+	m_team = team;
+}
+
+void Pillar::isValid(uint8_t coordX, uint8_t coordY)
+{
+	if (m_coordX > coordX || m_coordY > coordY) {
+		std::cerr << "Invalid pillar";
+		return;
+	}
+}
