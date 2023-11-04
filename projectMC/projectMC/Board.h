8386ce23dda124c1b5df5b455a26675e5d03260c
FilipS14 <filipsirboiu@yahoo.ro>
2023-11-04 15:10:46 +0200

    Am creat header-ul Board: -constructori -destructori -geteri -seteri

---
+++ projectMC/projectMC/Board.h
@@ -0,0 +1,22 @@
+#pragma once
+#include<iostream>
+#include<cstdint>
+#include<vector>
+
+class Board
+{
+public:
+	Board(uint8_t line, uint8_t column);
+	Board() = default;
+	~Board() = default;
+	uint8_t getLine() const;
+	uint8_t getColumn() const;
+	void setLine(uint8_t line);
+	void setColumn(uint8_t column);
+	void drawMatrix(std::vector<std::vector< uint8_t>>& matrix);
+
+private:
+	uint8_t m_line, m_column;
+	std::vector<std::vector<uint8_t>>m_matrix;
+};
+
