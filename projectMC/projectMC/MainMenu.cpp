d601f2b7dad3bb06036740e5b48512edbfd3d634
FilipS14 <filipsirboiu@yahoo.ro>
2023-11-04 15:59:24 +0200

    Am adaugat 3 butoane la clasa MainMenu

--- projectMC/projectMC/MainMenu.cpp
+++ projectMC/projectMC/MainMenu.cpp
@@ -2,18 +2,30 @@
 #include <QPushButton>
 #include <QVBoxLayout>
 
-MainMenu::MainMenu(QWidget* parent) : QMainWindow(parent) 
-{
+
+MainMenu::MainMenu(QWidget* parent) : QMainWindow(parent) {
     setWindowTitle("Twixt Game - Meniu Principal");
-    resize(200, 300); 
+    resize(200, 300);
+
+    QVBoxLayout* layout = new QVBoxLayout;
+
+    startButton = new QPushButton("Începe jocul");
+    instructionsButton = new QPushButton("Instrucțiuni");
+    leaderboardButton = new QPushButton("LeaderBoard");
+
+    layout->addWidget(startButton);
+    layout->addWidget(instructionsButton);
+    layout->addWidget(leaderboardButton);
+
+    QWidget* centralWidget = new QWidget;
+    centralWidget->setLayout(layout);
+    setCentralWidget(centralWidget);  
 }
 
-void MainMenu::startGame() 
-{
+void MainMenu::startGame() {
 
 }
 
-void MainMenu::showInstructions() 
-{
+void MainMenu::showInstructions() {
 
 }
