6ab04d05bf936fcd79ee3f16d30391ce31753c93
FilipS14 <filipsirboiu@yahoo.ro>
2023-11-04 16:02:35 +0200

    Am imbunatatin designul butoanelor.

--- projectMC/projectMC/MainMenu.cpp
+++ projectMC/projectMC/MainMenu.cpp
@@ -9,13 +9,29 @@ MainMenu::MainMenu(QWidget* parent) : QMainWindow(parent) {
 
     QVBoxLayout* layout = new QVBoxLayout;
 
+    // Adaug un spatiu vertical in partea de sus a ferestrei
+    QWidget* topSpacer = new QWidget();
+    topSpacer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    layout->addWidget(topSpacer);
+
     startButton = new QPushButton("Începe jocul");
     instructionsButton = new QPushButton("Instrucțiuni");
     leaderboardButton = new QPushButton("LeaderBoard");
 
+    // Setez dimensiunile butoanelor
+    QSize buttonSize(200, 50);
+    startButton->setFixedSize(buttonSize);
+    instructionsButton->setFixedSize(buttonSize);
+    leaderboardButton->setFixedSize(buttonSize);
+
     layout->addWidget(startButton);
     layout->addWidget(instructionsButton);
     layout->addWidget(leaderboardButton);
+
+    // Adaug un spatiu vertical in partea de jos a ferestrei
+    QWidget* bottomSpacer = new QWidget();
+    bottomSpacer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    layout->addWidget(bottomSpacer);
 
     QWidget* centralWidget = new QWidget;
     centralWidget->setLayout(layout);
