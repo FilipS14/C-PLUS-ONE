1509b5f02c3dce318cc3123c66b4c798a1dd5833
FilipS14 <filipsirboiu@yahoo.ro>
2023-11-11 16:00:23 +0200

    Am implementat functia onCellClicked din clasa BoardWidget.

--- projectMC/projectMC/BoardWidget.cpp
+++ projectMC/projectMC/BoardWidget.cpp
@@ -8,3 +8,19 @@ void BoardWidget::setIsBlack(bool isBlack) {
 	m_isBlack = isBlack;
 }
 
+void BoardWidget::onCellClicked()
+{
+	QPushButton* clickedButton = qobject_cast<QPushButton*>(sender());
+	if (clickedButton)
+	{
+		if (getIsBlack())
+		{
+			clickedButton->setStyleSheet("background-color: black; border: 1px solid black; border-radius: 5px;");
+		}
+		else
+		{
+			clickedButton->setStyleSheet("background-color: red; border: 1px solid black; border-radius: 5px;");
+		}
+		setIsBlack(!getIsBlack());
+	}
+}
