21e903113b9ae56a5aab79eb8f414f2f699f920e
FilipS14 <filipsirboiu@yahoo.ro>
2023-11-11 15:47:26 +0200

    Am facut cateva modificari in header-ul BoardWidget: -am adugat variablia isBlack -am facut geteri si seteri pentru isBlack -am adaugat 2 functii -am schimbat denumirea la 2 variabile

--- projectMC/projectMC/BoardWidget.h
+++ projectMC/projectMC/BoardWidget.h
@@ -8,11 +8,16 @@ class BoardWidget : public QMainWindow
 	Q_OBJECT
 public:
 	BoardWidget(QWidget* parent = nullptr);
-	~BoardWidget();
+	~BoardWidget() = default;
+	bool getIsBlack() const;
+	void setIsBlack(bool isBlack);
 	void onCellClicked();
 	void resetBoard();
+	void displayGameOverMessage();
+	void removePiece(int row, int col);
 
 private:
-	QGridLayout* gridLayout;
-	QList<QList<QPushButton*>>boardCells;
+	QGridLayout* m_gridLayout;
+	QList<QList<QPushButton*>>m_boardCells;
+	bool m_isBlack = false;//pentru a incepe sa coloreze cu rosu
 };
