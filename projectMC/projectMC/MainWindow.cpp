ed500f161252c8700548fccced5b7ef542e1d7c9
FilipS14 <filipsirboiu@yahoo.ro>
2023-11-19 16:38:36 +0200

    Am creat clasa MainWindow

---
+++ projectMC/projectMC/MainWindow.cpp
@@ -0,0 +1,18 @@
+#include "MainWindow.h"
+
+MainWindow::MainWindow(QWidget* parent) :
+	QMainWindow{ parent } {
+
+}
+
+MainWindow::~MainWindow() {
+	
+}
+
+void MainWindow::startGame() {
+
+}
+
+void MainWindow::showInstructions() {
+
+}
