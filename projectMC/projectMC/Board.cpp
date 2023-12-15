0ba3f5409f7610a3bd1874354dad09de59fde7ac
FilipS14 <filipsirboiu@yahoo.ro>
2023-12-15 18:56:54 +0200

    Am implementat Move Assignament Constructor Am implementat Copy Assignament Operator

--- projectMC/projectMC/Board.cpp
+++ projectMC/projectMC/Board.cpp
@@ -12,12 +12,32 @@ Board::Board(Board&& other) noexcept : //Move Constructor
 	m_board{ std::move(other.m_board) }
 {}
 
+Board& Board::operator=(Board&& other) noexcept //Move Assignament Constructor
+{
+	if (this != &other) {
+		m_line = std::exchange(other.m_line, 0);
+		m_column = std::exchange(other.m_column, 0);
+		m_board = std::move(other.m_board);
+	}
+	return *this;
+}
+
 Board::Board(const Board& other) : //Copy Constructor
 	m_line{ other.m_line },
 	m_column{ other.m_column },
 	m_board{ other.m_board }
 {}
 
+Board& Board::operator=(const Board& other) //Copy Assignament Operator
+{
+	if (this != &other) {
+		m_board.clear();
+		m_line = other.m_line;
+		m_column = other.m_column;
+		m_board = other.m_board;
+	}
+	return *this;
+}
 
 uint8_t Board::getLine() const {
 	return m_line;
