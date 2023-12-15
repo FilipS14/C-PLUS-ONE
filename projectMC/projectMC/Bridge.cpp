1982fd8bbf9c40c980e9cf6da5c38bbfcf9e8b7b
FilipS14 <filipsirboiu@yahoo.ro>
2023-12-15 17:26:33 +0200

    Am implementat move assignment operator, copy assignment operator .

--- projectMC/projectMC/Bridge.cpp
+++ projectMC/projectMC/Bridge.cpp
@@ -10,7 +10,26 @@ Bridge::Bridge(Bridge&& other) noexcept //Move Constructor
 	m_end{ std::move(other.m_end) }
 {}
 
+Bridge& Bridge::operator=(Bridge&& other) noexcept //Move Assignment Operator
+{
+	if (this != &other)
+	{
+		m_start = std::move(other.m_start);
+		m_end = std::move(other.m_end);
+	}
+	return *this;
+}
+
 Bridge::Bridge(const Bridge& other) //Copy Constructor
 	: m_start{ other.m_start },
 	m_end{ other.m_end }
 {}
+
+Bridge& Bridge::operator=(const Bridge& other) //Copy Assignment Operator 
+{
+	if (this != &other) {
+		m_start = other.m_start;
+		m_end = other.m_end;
+	}
+	return *this;
+}
