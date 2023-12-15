bc5dd149335c1008d2f8f612038b147cb821086a
FilipS14 <filipsirboiu@yahoo.ro>
2023-12-15 16:09:05 +0200

    Am modificat clasa pillar. -Tin minte un QPoint decat 2 coordoante de x si y -Am adaugat rules of 5 momentan

--- projectMC/projectMC/Pillar.h
+++ projectMC/projectMC/Pillar.h
@@ -1,27 +1,36 @@
 #pragma once
 #include <iostream>
-#include<cstdint>
+#include <cstdint>
+#include <QPoint>
 
 enum class Team { none, red, black };
 
 class Pillar {
-private:
-	uint8_t m_coordX, m_coordY;
-	Team m_team;
-	bool m_isConnected;
 
 public:
-	Pillar(uint8_t coordX, uint8_t coordY, Team team);
-	Pillar(const Pillar& p);
-	~Pillar() = default;
-	uint8_t getCoordX() const;
-	uint8_t getCoordY() const;
-	uint8_t getIsConnected() const;
-	Team getTeam() const;
-	void setCoordX(uint8_t coordX);
-	void setCoordY(uint8_t coordY);
-	void setIsConneted(uint8_t isConnected);
-	void setTeam(Team team);
-	void isValid(uint8_t x, uint8_t y);
+	//rule of 5
+	Pillar(QPoint coordinates, Team team); //Constructor
+	Pillar() = default; //Constructor
+	Pillar(Pillar&& other) noexcept; //Move Constructor
+	Pillar& operator=(Pillar&& other) noexcept; //Move Assignament Constructor
+	Pillar(const Pillar& p); //Copy Constructor
+	Pillar& operator=(const Pillar& other); //Copy Assignament Oprator 
+	~Pillar() = default; //Destructor
+
+	//overloading operators
+	bool operator==(const Pillar& other) const;
 	
+	//Getteri
+	QPoint getCoordinates() const;
+	Team getTeam() const;
+	
+	//Setteri
+	void setCoordinates(const QPoint& coordinates);
+	void setTeam(Team team);
+	void isValid(int32_t x, int32_t y);
+
+
+private:
+	QPoint m_coordinates;
+	Team m_team;
 };
