4e2bf99311bbfdafefd67e4c6ae5aec0c2a1206e
FilipS14 <filipsirboiu@yahoo.ro>
2023-12-15 16:11:46 +0200

    -Am modificat constructorul -Am implementat move constructor -am implementat move assignment constructor

--- projectMC/projectMC/Pillar.cpp
+++ projectMC/projectMC/Pillar.cpp
@@ -1,9 +1,23 @@
 #include "Pillar.h"
 
-Pillar::Pillar(uint8_t coordX, uint8_t coordY, Team team) :
-	m_coordX{ coordX },
-	m_coordY{ coordY },
-	m_team{ team } {
+Pillar::Pillar(QPoint coordinates, Team team) : //Constructor
+	m_coordinates{ coordinates },
+	m_team{ team }
+{}
+
+Pillar::Pillar(Pillar&& other) noexcept // Move Constructor
+	: m_coordinates(std::exchange(other.m_coordinates, QPoint())),
+	m_team(std::exchange(other.m_team, Team::none))
+{}
+
+
+Pillar& Pillar::operator=(Pillar&& other) noexcept  // Move Assignment Constructor
+{
+	if (this != &other) {
+		m_coordinates = std::exchange(other.m_coordinates, QPoint());
+		m_team = std::exchange(other.m_team, Team::none);
+	}
+	return *this;
 }
 
 Pillar::Pillar(const Pillar& p) :
