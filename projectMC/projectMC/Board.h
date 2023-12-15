077f1ddf61f0ef6ace1c9153faa1aff318bbf546
FilipS14 <filipsirboiu@yahoo.ro>
2023-12-15 18:01:37 +0200

    Am adus modificari in clasa Board -rule of 5 -retin o matrice de Cell.

--- projectMC/projectMC/Board.h
+++ projectMC/projectMC/Board.h
@@ -2,25 +2,40 @@
 #include<iostream>
 #include<cstdint>
 #include<vector>
+#include "Cell.h"
+#include "Pillar.h"
+#include "Bridge.h"
 
 class Board
 {
 public:
-	Board(uint8_t line, uint8_t column);
-	Board() = default;
-	~Board() = default;
+	//rule of 5
+	Board(uint8_t line, uint8_t column); //Constructor
+	Board() = default; //Constructor
+	Board(Board&& other) noexcept; //Move Constructor
+	Board& operator=(Board&& other)noexcept; // Move Assignament Constructor
+	Board(const Board& other); //Copy Constructor
+	Board& operator=(const Board& other); //Copy Assignament Operator
+	~Board() = default; //Destructor
+
+	//Getteri
 	uint8_t getLine() const;
 	uint8_t getColumn() const;
-	uint8_t getValue(uint8_t line, uint8_t column) const;
+	std::vector<std::vector<Cell>>& getMatrix();
+
+	//Setteri
 	void setLine(uint8_t line);
 	void setColumn(uint8_t column);
-	void setValue(uint8_t line, uint8_t column, uint8_t value);
-	bool isOccupied(uint8_t line, uint8_t column);
-	bool isValidMove(uint8_t line, uint8_t column, bool isBlack);
-	void reset();
+	
+	//Matrix manipulation
+	void addCell(size_t line, size_t column, QPoint coordinates);
+	Cell getCell(uint32_t x, uint32_t y) const;
+	void resizeBoard(size_t line, size_t column);
+	void addPillar(const Pillar& pillar);
+	void addBridge(const Bridge& bridge);
 
 private:
 	uint8_t m_line, m_column;
-	std::vector<std::vector<uint8_t>>m_matrix;
+	std::vector<std::vector<Cell>>m_board;
 };
 
