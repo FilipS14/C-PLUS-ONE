9a6129609d4f0af2b2e6183a10ee84a4f61d00c5
FilipS14 <filipsirboiu@yahoo.ro>
2023-12-23 13:39:44 +0200

    Am adus modificari in header-ul player

--- projectMC/projectMC/Board.h
+++ projectMC/projectMC/Board.h
@@ -11,6 +11,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <memory>
+#include <array>
 
 struct TwoPoint {
 	QPoint point1;
--- projectMC/projectMC/Player.h
+++ projectMC/projectMC/Player.h
@@ -2,26 +2,40 @@
 #include <iostream>
 #include <string>
 
+enum class Team
+{
+	red, black, none
+};
+
 class Player
 {
 private:
 	std::string m_name;
-	std::string m_color;
-	bool m_status;
-	bool m_player1_turn;
+	Team m_team;
+	int8_t m_numberOfPillars;
+	int8_t m_numbersOfBridges;
+	bool m_movePillar;
 
 public:
 	Player() = default;
-	Player(std::string name, std::string color, bool status, bool player1_turn);
+	Player(std::string name, Team team);
 	~Player() = default;
-	std::string getName() const;
-	std::string getColor() const;
-	bool get_status() const;
-	void setName(std::string name);
-	void setColor(std::string color);
-	void displaiInfo();
-	bool player_won();
-	void setTurn(bool turn);
-	bool getTurn() const;
 
+	//GETTERS
+	std::string getName() const;
+	Team getTeam() const;
+	uint8_t getNumberOfPillars() const;
+	uint8_t getNumberOfBridges() const;
+	bool getMovePillar() const;
+
+	//SETTERS
+	void setName(std::string name);
+	void setTeam(Team team);
+	void setNumberOfPillars(uint8_t number);
+	void setNumberOfBridges(uint8_t number);
+	void setMovePillar(bool move);
+
+	//PLAYER HANDLING
+	void updateNumberOfPillars(uint8_t number);
+	void updateNumberOfBridges(uint8_t number);
 };
