4e91d39cb22498dc1fb0158da8eeb50fc22ec0f8
FilipS14 <filipsirboiu@yahoo.ro>
2023-12-23 13:47:26 +0200

    Am adus o modificare in clasa Bridge. Pentru fiecare pod retinem o culoare. Am facut modificarile necesare pentru acesta

--- projectMC/projectMC/Bridge.cpp
+++ projectMC/projectMC/Bridge.cpp
@@ -1,58 +1,72 @@
 #include "Bridge.h"
 
-Bridge::Bridge(QPoint start, QPoint end) : //Constructor
-	m_start{ start },
-	m_end{ end }
+Bridge::Bridge(QPoint start, QPoint end, QColor color) : //Constructor
+    m_start{ start },
+    m_end{ end },
+    m_color{ color }
 {}
 
 Bridge::Bridge(Bridge&& other) noexcept //Move Constructor
-	: m_start{ std::move(other.m_start) },
-	m_end{ std::move(other.m_end) }
+    : m_start{ std::move(other.m_start) },
+    m_end{ std::move(other.m_end) },
+    m_color{ std::move(other.m_color) }
 {}
 
 Bridge& Bridge::operator=(Bridge&& other) noexcept //Move Assignment Operator
 {
-	if (this != &other)
-	{
-		m_start = std::move(other.m_start);
-		m_end = std::move(other.m_end);
-	}
-	return *this;
+    if (this != &other)
+    {
+        m_start = std::move(other.m_start);
+        m_end = std::move(other.m_end);
+        m_color = std::move(other.m_color);
+    }
+    return *this;
 }
 
 Bridge::Bridge(const Bridge& other) //Copy Constructor
-	: m_start{ other.m_start },
-	m_end{ other.m_end }
+    : m_start{ other.m_start },
+    m_end{ other.m_end },
+    m_color{ other.m_color }
 {}
 
 Bridge& Bridge::operator=(const Bridge& other) //Copy Assignment Operator 
 {
-	if (this != &other) {
-		m_start = other.m_start;
-		m_end = other.m_end;
-	}
-	return *this;
+    if (this != &other) {
+        m_start = other.m_start;
+        m_end = other.m_end;
+        m_color = other.m_color;
+    }
+    return *this;
 }
 
 //Getteri
 QPoint Bridge::getCoordinatesStart() const
 {
-	return m_start;
+    return m_start;
 }
 
 QPoint Bridge::getCoordinatesEnd() const
 {
-	return m_end;
+    return m_end;
+}
+
+QColor Bridge::getColor() const
+{
+    return m_color;
 }
 
 //Setteri
 void Bridge::setCoordinatesStart(const QPoint& start)
 {
-	m_start = start;
+    m_start = start;
 }
 
 void Bridge::setCoordinatesEnd(const QPoint& end)
 {
-	m_end = end;
+    m_end = end;
 }
 
+void Bridge::setColor(const QColor& color)
+{
+    m_color = color;
+}
--- projectMC/projectMC/Bridge.h
+++ projectMC/projectMC/Bridge.h
@@ -1,14 +1,17 @@
 #pragma once
 #include<iostream>
+#include<utility>
 #include <cstdint>
 #include <QPoint>
+#include <QColor>
+#include "Pillar.h"
 
 class Bridge
 {
 public:
 	//rule of 5
 	Bridge() = default;
-	Bridge(QPoint start, QPoint end); //Constructor
+	Bridge(QPoint start, QPoint end, QColor color); //Constructor
 	Bridge(Bridge&& other) noexcept; //Move Constructor
 	Bridge& operator=(Bridge&& other)noexcept; // Move Assignament Constructor
 	Bridge(const Bridge& other); //Copy Constructor
@@ -18,11 +21,14 @@ public:
 	//Getteri
 	QPoint getCoordinatesStart() const;
 	QPoint getCoordinatesEnd() const;
+	QColor getColor() const;
 
 	//Setteri
 	void setCoordinatesStart(const QPoint& start);
 	void setCoordinatesEnd(const QPoint& end);
+	void setColor(const QColor& color);
 
 private:
 	QPoint m_start, m_end;
+	QColor m_color;
 };
