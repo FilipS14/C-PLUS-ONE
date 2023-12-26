9facf4a9a13ac8d4adcf724a7ff006cb22b6cc64
FilipS14 <filipsirboiu@yahoo.ro>
2023-12-26 17:30:30 +0200

    Am implementat functia checkWinCondition

--- projectMC/projectMC/Game.cpp
+++ projectMC/projectMC/Game.cpp
@@ -19,3 +19,22 @@ Player& Game::getBlackPlayer() const
 Player& Game::getCurrentPlayer() const {
     return *m_currentPlayer;
 }
+
+//GAME HANDLER
+bool Game::checkWinCondition(Player& player) {
+    const int dX[8]{ -2, -2, -1, -1, 1, 1, 2, 2 }, dY[8]{ -1, +1, -2, +2, -2, +2, -1, +1 };
+    std::unordered_set<Cell, CellHash, CellEqual> visitedCells;
+    std::vector<Cell> vectorBase;
+
+    vectorBase = (player.getTeam() == Team::red) ? m_board->getRedBase() : m_board->getBlackBase();
+
+    for (const auto& baseCell : vectorBase) {
+        if (baseCell.getOcupier()) {
+            visitedCells.emplace(baseCell);
+            if (dfsTraversal(player, baseCell, visitedCells, dX, dY)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
