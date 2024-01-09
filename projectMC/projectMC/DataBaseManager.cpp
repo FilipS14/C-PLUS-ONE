2a69f0ea09621f07ad7f53d9b409875d42b80a38
FilipS14 <filipsirboiu@yahoo.ro>
2024-01-09 03:18:55 +0200

    Am implementat functiile createTable si updatePlayerStats

--- projectMC/projectMC/DataBaseManager.cpp
+++ projectMC/projectMC/DataBaseManager.cpp
@@ -13,3 +13,20 @@ DataBaseManager::DataBaseManager() {
         createTable();
     }
 }
+
+void DataBaseManager::createTable() {
+    QSqlQuery query;
+    if (!query.exec("CREATE TABLE IF NOT EXISTS player_stats (id INTEGER PRIMARY KEY, username TEXT UNIQUE, wins INTEGER, losses INTEGER)")) {
+        qDebug() << "Eroare la crearea tablei:" << query.lastError().text();
+    }
+}
+
+void DataBaseManager::updatePlayerStats(const QString& playerName, int wins, int losses)
+{
+    QSqlQuery query;
+    query.prepare("INSERT OR REPLACE INTO player_stats (username, wins, losses) VALUES (?, ?, ?)");
+    query.bindValue(0, playerName);
+    query.bindValue(1, wins);
+    query.bindValue(2, losses);
+    query.exec();
+}
